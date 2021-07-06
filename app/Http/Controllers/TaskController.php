97ad47efbaea5b2d938869bd84e85fe8a68c9bf3
Huu Tuan <huutuandn96@gmail.com>
2021-07-06 13:47:35 +0700

    todo function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\TaskRequest;
 
 class TaskController extends Controller
 {
     public function index()
+    {   
+        $tasks = Task::all();
+
+        return view('task', compact('tasks'));
+    }
+
+    public function store(TaskRequest $request)
     {
-        return view('task');
+        $tasks = new Task();
+        $tasks->name = $request->name;
+        $tasks->save();
+
+        return redirect()->route('task.index')->with('success', 'Add task successfully');
+    }
+
+    public function destroy(Task $task) 
+    {
+        $task->delete();
+
+        return redirect()->route('task.index')->with('success', 'Delete task successfully');
     }
 }
