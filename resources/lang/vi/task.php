0230e55ef75cab72de034593298efe3d87ec8aed
Huu Tuan <huutuandn96@gmail.com>
2021-07-06 09:30:05 +0700

    show task view

---
+++ resources/lang/vi/task.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'newtask' => 'Công việc mới',
+    'addtask' => 'Thêm công việc',
+    'task' => 'Công việc',
+    
+];
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+
+        <!-- CSS And JavaScript -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/a90+nuLcll.min.css" integrity="sha512-iBBXm8fWSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
+        <!-- CSS-->
+        <base href="{{ asset('') }}">
+        <link rel="stylesheet" href="asset/css/style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
